cf8c0ada19bad37acb1a8fab0a594296d17ce107
Esteban Quesada Silva <esteban4565@gmail.com>
2015-05-18 06:34:14 -0600

    parametros hosting
    http://www.pruebasesteban.bl.ee/

--- config.php
+++ config.php
@@ -1,13 +1,29 @@
 <?php
 //estaba debajo de la definicion URL
 define('LIBS','libs/'); 
-define('URL','http://localhost/sipce/');
 
+//variables cuando uso localhost
+define('URL','http://localhost/sipce/');
 define('DB_TYPE','mysql');
 define('DB_HOST','localhost');
 define('DB_NAME','db_sipce');
 define('DB_USER','root');
 define('DB_PASS','');
+
+//variables cuando uso hostinger.com
+//define('URL', 'http://www.pruebasesteban.bl.ee/');
+//define('DB_TYPE', 'mysql');
+//define('DB_HOST', 'mysql.hostinger.es');
+//define('DB_NAME', 'u276605033_mvc');
+//define('DB_USER', 'u276605033_eqs');
+//define('DB_PASS', '63246884565');
+
+
+//Usuario FTP FileZilla
+//Servidor: 31.170.164.163
+//Usuario: u276605033
+//Contraseña: 63246884565
+//Puerto: 21
 
 /*No cambiar la hash_key porque es la clave del password*/
 define('HASH_GENERAL_KEY','ctpcarrizalalajuela2');
