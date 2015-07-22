00a545a2e015e4e60f3adfb53ca85348327f9047
Esteban Quesada Silva <esteban4565@gmail.com>
2015-07-21 21:16:00 -0600

    Usuario Admin
    sql con insert para usuario admin

---
+++ Bases de datos/usuarioAdmin.sql
@@ -0,0 +1,3 @@
+/*Usuario Admin*/
+INSERT INTO `sipce_persona` (`cedula`, `tipoUsuario`, `passwords`, `nombre`, `apellido1`, `apellido2`) VALUES
+('admin', 1, '1e576d46979395c3edc99e69e859864b', 'Esteban', 'Quesada', 'Silva');
