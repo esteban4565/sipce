c49cf55f18a680f7eaef74c90c48401b6edb49b4
Esteban Quesada Silva <esteban4565@gmail.com>
2017-12-10 21:34:46 -0600

    Configuración de secciones
    Configuración de secciones

--- views/seccion/configSecciones.php
+++ views/seccion/configSecciones.php
@@ -215,6 +215,7 @@
             <div class="col-xs-3 col-md-3">
                 <select class="form-control input-sm" name="sl_CantidadSecciones" id="sl_CantidadSecciones">
                     <option value="">Seleccione</option>
+                    <option value="0">0</option>
                     <option value="1">1</option>
                     <option value="2">2</option>
                     <option value="3">3</option>
